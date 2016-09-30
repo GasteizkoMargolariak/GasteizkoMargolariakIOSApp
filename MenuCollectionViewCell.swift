0382149d6380cf5e81b3e12e68e1236c0cb48bc1
Inigo Valentin <seavenois@inigos-macbook-pro.local>
2016-09-30 19:36:14 +0200

    First prototype for the section menu.

---
+++ MenuCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCollectionViewCell.swift
+//  app
+//
+//  Created by Inigo Valentin on 30/09/16.
+//  Copyright © 2016 Margolariak. All rights reserved.
+//
+
+import UIKit
+class MenuCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+}
---
+++ app/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Inigo Valentin on 28/09/16.
+//  Copyright © 2016 Margolariak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var items = ["Home", "Location", "La Blanca", "Actividades", "Blog", "Gallery"]
+    
+    
+    // MARK: - UICollectionViewDataSource protocol
+    
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MenuCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.myLabel.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+
+
+}
+
