26f94afc819f56dfe9b06459b7412b041091328d
Iñigo Valentin <seavenois@MacBook-Air-de-Maria.local>
2016-11-21 16:10:14 +0100

    Created section placeholder.

---
+++ app/UIView.swift
@@ -0,0 +1,42 @@
+//
+//  UIView.swift
+//  app
+//
+//  Created by Iñigo Valentin on 21/11/16.
+//  Copyright © 2016 Margolariak. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable extension UIView {
+    @IBInspectable var borderColor:UIColor? {
+        set {
+            layer.borderColor = newValue!.cgColor
+        }
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor:color)
+            }
+            else {
+                return nil
+            }
+        }
+    }
+    @IBInspectable var borderWidth:CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    @IBInspectable var cornerRadius:CGFloat {
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
