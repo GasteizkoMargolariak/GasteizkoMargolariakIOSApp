98c373a38a354c3e1dd0af6f1e978b61ba1df662
Iñigo Valentin <seavenois@MacBook-Air-de-Maria.local>
2016-11-21 18:47:25 +0100

    Fixed the home screen scrollview

--- app/HomeView.swift
+++ app/HomeView.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 class HomeView: UIView {
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var container: UIView!
     @IBOutlet weak var locationSection: Section!
     @IBOutlet weak var lablancaSection: Section!
@@ -28,6 +29,8 @@ class HomeView: UIView {
         self.addSubview(container)
         container.frame = self.bounds
         
+        
+        
         //Set titles
         locationSection.setTitle(text: "Encuentranos")
         lablancaSection.setTitle(text: "La Blanca")
@@ -36,5 +39,27 @@ class HomeView: UIView {
         gallerySection.setTitle(text: "Ultimas fotos")
         pastActivitiesSection.setTitle(text: "Ultimas actividades")
         socialSection.setTitle(text: "Siguenos")
+        
+        //Always at the end: update scrollview
+        //var contentRect: CGRect = CGRect.zero;
+        //for view in scrollView.subviews {
+        //    contentRect = contentRect.union(view.frame);
+        //}
+        //self.scrollView.contentSize = contentRect.size;
+        
+        
+        var h: Int = 0
+        for view in scrollView.subviews {
+            //contentRect = contentRect.union(view.frame);
+            h = h + Int(view.frame.height) + 30 //Why 30?
+            print("curh: \(h)")
+        }
+        self.scrollView.contentSize.height = CGFloat(h);
+        
+        //let lastView : Section! = scrollView.subviews.last as! Section!
+        //let height = lastView.frame.size.height
+        //let pos = lastView.frame.origin.y
+        //let sizeOfContent = height + pos + 10
+        //scrollView.contentSize.height = sizeOfContent
     }
 }
